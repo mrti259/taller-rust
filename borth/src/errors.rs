5a104b7bbd16a23d6a3f43f7d00fd49599b862f3
Borja Garibotti <mgaribotti@fi.uba.ar>
2025-03-24 17:45:05 -0300

    Print error in kebabcase

--- borth/src/errors.rs
+++ borth/src/errors.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 #[derive(Debug)]
 pub enum BorthError {
     MissingArguments,
@@ -7,4 +9,29 @@ pub enum BorthError {
     CanNotReadCode,
     CanNotWriteFile,
     RuntimeError,
+}
+
+impl Display for BorthError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if f.alternate() {
+            write!(f, "{:?}", self)
+        } else {
+            let name = format!("{:#}", self);
+            f.write_str(&to_kebabcase(name))
+        }
+    }
+}
+
+fn to_kebabcase(str: String) -> String {
+    str.chars()
+        .map(|c| {
+            if c.is_uppercase() {
+                format!("-{}", c.to_lowercase())
+            } else {
+                c.to_string()
+            }
+        })
+        .collect::<String>()
+        .trim_start_matches("-")
+        .to_string()
 }
